a9fa93ab64b88e3cf7f2449807eb1a0708e0c674
Enes2007 <enestasdemir5443@gmail.com>
2025-01-11 02:00:48 +0100

    Initial Commit

---
+++ multi-calc/src/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import * as ReactDOM from "react-dom";
+
+export default class Home extends React.Component {
+
+
+
+  render() {
+    return <div>Basic Calculator</div>;
+  }
+}
